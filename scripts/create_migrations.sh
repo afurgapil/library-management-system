10c18666c2d5c0196fc5660412bd8bbc0d5a2404
Gapil <gapilsoft@gmail.com>
2024-06-22 04:11:50 +0300

    ci

--- scripts/create_migrations.sh
+++ scripts/create_migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE TABLE IF NOT EXISTS public.book (
+DATABASE_URL="postgres://postgres:${{ secrets.POSTGRES_PASSWORD }}@localhost:5432/librarymanagementsystem_test"
+psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -w <"$DATABASE_URL" -c "CREATE TABLE IF NOT EXISTS public.book (
         book_id character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         title character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         author character varying(255) COLLATE pg_catalog.'default' NOT NULL,
@@ -14,9 +14,8 @@ psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE
         donor character varying(255) COLLATE pg_catalog.'default',
         CONSTRAINT book_pkey PRIMARY KEY (book_id)
     ) TABLESPACE pg_default;"
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "ALTER TABLE IF EXISTS public.book OWNER to postgres;"
 
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE TABLE IF NOT EXISTS public.employee (
+psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -w <"$DATABASE_URL" -c "CREATE TABLE IF NOT EXISTS public.employee (
         employee_id character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         employee_mail character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         employee_username character varying(255) COLLATE pg_catalog.'default' NOT NULL,
@@ -25,9 +24,8 @@ psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE
         employee_password character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         CONSTRAINT employee_pkey PRIMARY KEY (employee_id)
     ) TABLESPACE pg_default;"
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "ALTER TABLE IF EXISTS public.employee OWNER to postgres;"
 
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE TABLE IF NOT EXISTS public.student (
+psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -w <"$DATABASE_URL" -c "CREATE TABLE IF NOT EXISTS public.student (
         student_id character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         student_mail character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         student_password character varying(255) COLLATE pg_catalog.'default' NOT NULL,
@@ -36,9 +34,8 @@ psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE
         is_banned boolean NOT NULL DEFAULT false,
         CONSTRAINT student_pkey PRIMARY KEY (student_id)
     ) TABLESPACE pg_default;"
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "ALTER TABLE IF EXISTS public.student OWNER to postgres;"
 
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE TABLE IF NOT EXISTS public.book_borrow (
+psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -w <"$DATABASE_URL" -c "CREATE TABLE IF NOT EXISTS public.book_borrow (
         borrow_id character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         student_id character varying(255) COLLATE pg_catalog.'default' NOT NULL,
         book_id character varying(255) COLLATE pg_catalog.'default' NOT NULL,
@@ -57,4 +54,3 @@ psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "CREATE
             ON DELETE CASCADE
             NOT VALID
     ) TABLESPACE pg_default;"
-psql -h localhost -p 5432 -U postgres -d librarymanagementsystem_test -c "ALTER TABLE IF EXISTS public.book_borrow OWNER to postgres;"
